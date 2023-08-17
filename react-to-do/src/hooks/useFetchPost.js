f7e3938c3d923936e0d87b8ba9daf0907fb1ba80
nghiavt <nghiavt@avada.email>
2023-08-17 09:43:52 +0700

    add todo

--- react-to-do/src/hooks/useFetchPost.js
+++ react-to-do/src/hooks/useFetchPost.js
@@ -1,25 +1,30 @@
-import axios from 'axios';
-import { useState } from 'react'
+import axios from "axios";
+import { useState } from "react";
 
 export function useFetchPost(url) {
-    const [creating, setCreating] = useState(false);
-    const handleCreate = async({data ={}, createSuccess=()=>{}, createError=()=>{}})=>{
-        try {
-            setCreating(true);
-            const res = await axios.post(url, data);
-            const dataResponse = res.data;
-            if(dataResponse.success){
-                const {data} = dataResponse;
-                await createSuccess(data);
-            }
-            if(dataResponse.error){
-                await createError();
-            }
-        } catch (error) {
-            console.error(error.message);
-        }finally{
-            setCreating(false);
-        }
+  const [creating, setCreating] = useState(false);
+  const handleCreate = async ({
+    data = {},
+    createSuccess = () => {},
+    createError = () => {},
+  }) => {
+    try {
+      setCreating(true);
+      const res = await axios.post(url, data);
+      const dataResponse = res.data;
+      if (dataResponse.success) {
+        const { data } = dataResponse;
+        //@hoangtien chỗ này đâu cần await nhỉ. Nếu TH là Promise thì nên để ntn https://i.imgur.com/AVd1CMn.png
+        await createSuccess(data);
+      }
+      if (dataResponse.error) {
+        await createError();
+      }
+    } catch (error) {
+      console.error(error.message);
+    } finally {
+      setCreating(false);
     }
-    return {creating, handleCreate};
+  };
+  return { creating, handleCreate };
 }
